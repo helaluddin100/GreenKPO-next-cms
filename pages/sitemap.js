73ad22fd540d73f283bf7c9b0991985cae2a1b65
Helal Uddin <freelancerhelaluddin@gmail.com>
2024-08-27 00:43:20 +0600

    sitemap generate done

---
+++ pages/sitemap.js
@@ -0,0 +1,53 @@
+import { GetServerSideProps } from "next";
+
+const Sitemap = () => {};
+
+export const getServerSideProps = async ({ res }) => {
+  // Fetch static paths
+  const staticPaths = [
+    "",
+    "/about",
+    "/contact",
+    "/product",
+    "/faqs",
+    "/knowledge-hub",
+    "/faqs#contact",
+  ];
+
+  // Fetch dynamic paths from your API
+  const response = await fetch("https://admin.greenkpo.com/api/posts");
+  const { data: posts } = await response.json();
+
+  // Generate dynamic paths using the `slug` field
+  const dynamicPaths = posts.map((post) => `/${post.slug}`);
+
+  // Combine static and dynamic paths
+  const allPaths = [...staticPaths, ...dynamicPaths];
+
+  // Create sitemap XML structure
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${allPaths
+      .map((url) => {
+        return `
+          <url>
+            <loc>${`https://greenkpo.com/post${url}`}</loc>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+          </url>
+        `;
+      })
+      .join("")}
+  </urlset>`;
+
+  // Send the sitemap to the response
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
